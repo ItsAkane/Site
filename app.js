f079179bbbf7188d4da5231fdb8fa306a0d37bf3
ItsAkaNe <gabriel.kosvoski@outlook.com>
2025-04-24 09:29:41 -0300

    archive changes for test and feedback

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const sql = neon(process.env.DATABASE_URL);
 
 const requestHandler = async (req, res) => {
   const result = await sql`SELECT version()`;
-  const { version } = result[0];
+  const { version } = result[0];a
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end(version);
 };
